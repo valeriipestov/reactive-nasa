729f7ce38e0f8ddd425bd8323f1b63d77114af55
valeriyPestov <valeriy.pestov@yahoo.com>
2022-07-20 15:41:47 +0300

    initial commit

---
+++ src/main/java/com/example/reactivenasa/ReactiveNasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivenasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveNasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveNasaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivenasa/ReactiveNasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivenasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveNasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
